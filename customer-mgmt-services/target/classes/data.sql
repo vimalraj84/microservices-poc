b5845ed538884e7f31f1da519e28a6c717563c51
vimalraj <vimalraj84@users.noreply.github.com>
2021-03-30 09:33:30 -0400

    Delete customer-mgmt-services/target/classes directory

--- customer-mgmt-services/target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-insert into CUSTOMER(CUST_ID,FIRST_NAME,LAST_NAME,PHONE,EMAIL) values(10001,'Anna','Field','123456789','anna@yahoo.com');
-insert into CUSTOMER(CUST_ID,FIRST_NAME,LAST_NAME,PHONE,EMAIL) values(10002,'Emmy','Anders','223456789','emmy@yahoo.com');
-insert into CUSTOMER(CUST_ID,FIRST_NAME,LAST_NAME,PHONE,EMAIL) values(10003,'Amber','Heared','323456789','amber@yahoo.com');
-insert into CUSTOMER(CUST_ID,FIRST_NAME,LAST_NAME,PHONE,EMAIL) values(10004,'Nicole','Middleton','423456789','nicole@yahoo.com');
